35521d5a5fe1e4939bc9bc0758f63efb248bb425
andrewperezledo <andrewperezledo@gmail.com>
2025-02-05 19:19:04 -0500

    new live.py w/ open cv

--- liveFeed.py
+++ liveFeed.py
@@ -1,23 +1,27 @@
 import cv2
 from picamera2 import Picamera2
-
+#from time import sleep
 # Identify the camera index (optional, modify if needed)
 camera_index = 0  # Assuming the camera is at index 0
 
 # Initialize the PiCamera2 object
 picam2 = Picamera2()
-
 # Configure the camera for preview
-config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)})
-picam2.configure(config)
+preview_config = picam2.create_preview_configuration()
+# INSIDE LAST LINE FUNCTION: main={"format": "RGB888", "size": (640, 480)}
+#picam2.start_preview(Preview.QTGL)
+picam2.configure(preview_config)
 picam2.start()
+
+#picam2.configure(config)
+#picam2.start()
 
 while True:
     # Capture a frame from the camera
-    frame = picam2.capture_array()
+    frame_array = picam2.capture_array("main")
 
     # Display the frame using OpenCV
-    cv2.imshow("PiCamera2 Feed", frame)
+    cv2.imshow("PiCamera2 Feed", frame_array)
 
     # Check for user input to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
