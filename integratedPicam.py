fa2f066e92e4bf425da444c2ec59f227e64d5b47
andrewperezledo <andrewperezledo@gmail.com>
2025-04-16 19:56:44 -0400

    new serial for IP

--- integratedPicam.py
+++ integratedPicam.py
@@ -81,7 +81,7 @@ def detect_orange_ball(image):
 
             # try:
 
-            send_angle(xpos)
+            
                 
                 #time.sleep(1)  # Give time for transmission
             # except Exception as e:
@@ -105,7 +105,7 @@ while True:
     result_image = detect_orange_ball(frame_array)
 
     cv2.imshow('Ball', result_image)
-
+    send_angle(xpos)
     # send_angle(xpos)
     time.sleep(0.01)
     # Check for user input to quit
